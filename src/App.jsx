9f4329c6def69ab3bb93f91216e82b3408915fa0
Volodymyr Basok <volodymyrquo@gmail.com>
2023-11-19 17:10:05 +0200

    added project section

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,20 @@
-import {Route, BrowserRouter as Router,Routes} from 'react-router-dom'
-import Navbar from './components/Navbar'
-import {Home,About,Projects,Contact} from './pages'
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import { Home, About, Projects, Contact } from './pages';
 const App = () => {
-  return (
-    <main className='bg-slate-300/20'>
-          <Router>
-            <Navbar />
-            <Routes>
-                <Route path='/' element={<Home/>}/>
-                <Route path='/about' element={<About/>}/>
-                <Route path='/projects' element={<Projects/>}/>
-                <Route path='/contact' element={<Contact/>}/>
+    return (
+        <main className="bg-slate-300/20 h-[100vh]">
+            <Router>
+                <Navbar />
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/about" element={<About />} />
+                    <Route path="/projects" element={<Projects />} />
+                    <Route path="/contact" element={<Contact />} />
                 </Routes>
-          </Router>
-     </main>
-  )
-}
+            </Router>
+        </main>
+    );
+};
 
-export default App
+export default App;
